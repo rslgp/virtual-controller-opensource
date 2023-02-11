cdf2ffed52f316482b1e05783c076058bb79f8e1
rslgp <rslgp@cin.ufpe.br>
2023-02-11 09:33:37 -0300

    Update chatgpt-keyboard-axis-reifel.py

--- chatgpt-keyboard-axis-reifel.py
+++ chatgpt-keyboard-axis-reifel.py
@@ -72,14 +72,5 @@ keyboard_thread.start()
 mouse_thread = threading.Thread(target=read_mouse)
 mouse_thread.start()
 
-def link(uri, label=None):
-    if label is None: 
-        label = uri
-    parameters = ''
-
-    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
-    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
-
-    return escape_mask.format(parameters, uri, label)
 print("\nOpensource Virtual Controller for Zomboid by modder Reifel is running");
 print("PRESS F1 to exit");
